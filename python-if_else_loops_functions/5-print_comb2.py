296266f24d6c4578ffec4f12bf27305339fa1146
Fatma Ben Azouz <fatmabenazouz@Fatmas-Macbook-Air.local>
2023-02-09 03:43:15 +0200

    Task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
+for i in range(99):
+    print("{:02d}".format(i), end=", ")
 else:
-    print("{:02}".format(number), end=", ")
+    print("{:02d}".format(i + 1))
