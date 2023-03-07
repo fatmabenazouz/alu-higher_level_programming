bc45fd3d4343a31dbbc3c78e0f443748545fef38
Fatma Ben Azouz <fatmabenazouz@Fatmas-Macbook-Air.local>
2023-03-08 01:21:18 +0200

    Task 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
+
 def uniq_add(my_list=[]):
-    newList = set(my_list)
     result = 0
-    for number in newList:
-        result += number
-    return
+    for x in set(my_list):
+        result += x
+    return result
