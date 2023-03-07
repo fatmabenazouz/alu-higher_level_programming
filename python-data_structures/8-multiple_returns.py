d201a82d168664f71237124f7b8a86b054869758
Fatma Ben Azouz <fatmabenazouz@Fatmas-Macbook-Air.local>
2023-03-08 00:50:24 +0200

    Task 8

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    """ returns a tuple with the length of a string and its first character."""
+    tup = ()
+    length = len(sentence)
+    first = ""
+
+    if not sentence:
+        length = 0
+        first = None
+        tup = tup + (length, first)
+
+    else:
+        first = sentence[0]
+        tup = tup + (length, first)
+
+    return tup
