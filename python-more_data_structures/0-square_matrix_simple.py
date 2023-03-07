b0cec4c4d4f8afefb2b5016d752daf9dd83e5386
Fatma Ben Azouz <fatmabenazouz@Fatmas-Macbook-Air.local>
2023-03-08 01:12:54 +0200

    first commit

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
+
 def square_matrix_simple(matrix=[]):
-    newMatrix = [[x ** 2 for x in row] for row in matrix]
-    return 
+    return ([list(map(lambda x: x * x, row)) for row in matrix]) 
