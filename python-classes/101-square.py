ce1d6a0df18a78847957382ceb875945775cb5f3
Fatma Ben Azouz <fatmabenazouz@Fatmas-Macbook-Air.local>
2023-03-17 23:19:56 +0200

    Task 8

--- python-classes/101-square.py
+++ python-classes/101-square.py
@@ -17,7 +17,7 @@ class Square:
         self.position = position
 
     @property
-    def size(self:
+    def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
 
